0e07f7acac6a1606a28decebcb5aa93aec97b06c
dhdenvo <dhd@rogers.com>
2023-07-30 05:00:04 -0400

    Remove the first page route as this can be done better with the other routes

--- server/features/library/libraryController.js
+++ server/features/library/libraryController.js
@@ -3,16 +3,11 @@ const pageModel = require("./pageModel");
 
 const getBooks = () => bookModel.findMany();
 const getBook = ({ params }) => bookModel.findOne(params.bookId, true);
-const getFirstPage = ({ params }) =>
-  getBook({ params })
-    .populate("firstPage")
-    .then(({ firstPage }) => firstPage);
 const getPage = ({ params }) =>
   pageModel.findOne(params.pageId, true).populate("nextPages");
 
 module.exports = {
   getBooks,
   getBook,
-  getFirstPage,
   getPage,
 };
