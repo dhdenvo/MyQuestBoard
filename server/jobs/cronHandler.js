ca442b1d2203492778e5e577f0a99a2585acea92
dhdenvo <dhd@rogers.com>
2023-08-01 23:03:51 -0400

    Add runtime to the quest failure cron job

--- server/jobs/cronHandler.js
+++ server/jobs/cronHandler.js
@@ -1,11 +1,14 @@
 const { CronJob } = require("cron");
 
 const CRONS = [
-  { func: require("./cronFuncs/questFailure"), runtime: null },
+  {
+    func: require("./cronFuncs/questFailure"),
+    runtime: "0 50 23 * * *",
+  },
   { func: require("./cronFuncs/questReminder"), runtime: null },
 ];
 
 CRONS.map(({ func, runtime }) => {
   if (!runtime) return;
-  new CronJob(runtime, func, null, true);
+  new CronJob(runtime, func, null, true, "America/Toronto");
 });
