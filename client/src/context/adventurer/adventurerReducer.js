7a00a3f7c0ec6c48b210a1d325133e837289bbcd
dhdenvo <dhd@rogers.com>
2023-08-08 15:14:18 -0400

    Fix the adventurer reducer to return a new state

--- client/src/context/adventurer/adventurerReducer.js
+++ client/src/context/adventurer/adventurerReducer.js
@@ -1,6 +1,8 @@
 export default function AdventurerReducer(state, action) {
   if (!action?.appliesTo) return;
   const { appliesTo, data } = action;
-  state[appliesTo] = data;
-  return state;
+  return {
+    ...state,
+    [appliesTo]: data,
+  };
 }
