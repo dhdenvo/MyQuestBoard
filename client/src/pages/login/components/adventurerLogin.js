f2576bc38c9c408361b12a0d29fd4043df42b99d
dhdenvo <dhd@rogers.com>
2023-08-08 14:02:50 -0400

    Add an on click to the adventurer login

--- client/src/pages/login/components/adventurerLogin.js
+++ client/src/pages/login/components/adventurerLogin.js
@@ -1,15 +1,21 @@
-import { Typography } from "@mui/material";
+import { Card, Typography } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 export default function AdventurerLogin({ adventurer }) {
   console.log("adventurer", adventurer);
   return (
     <div className={`adventurer-${adventurer.name}`}>
-      <Avatar
-        alt={adventurer.name}
-        src="https://rare-gallery.com/thumbnail/397179-wallpaper-rimuru-tempest-tensei-shitara-slime-datta-ken.jpg"
-        sx={{ width: 256, height: 256 }}
-      />
-      <Typography variant="h3">{adventurer.name}</Typography>
+      <Card
+        variant="outlined"
+        onClick={() => console.log("SUPPP")}
+        sx={{ minWidth: 256 }}
+      >
+        <Avatar
+          alt={adventurer.name}
+          src="https://rare-gallery.com/thumbnail/397179-wallpaper-rimuru-tempest-tensei-shitara-slime-datta-ken.jpg"
+          sx={{ width: 256, height: 256 }}
+        />
+        <Typography variant="h3">{adventurer.name}</Typography>
+      </Card>
     </div>
   );
 }
