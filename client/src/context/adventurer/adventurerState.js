1f882f9ac6a5f41b426b6880f1ec6bd12e73dff2
dhdenvo <dhd@rogers.com>
2023-08-08 15:14:55 -0400

    Load all adventurers on page load

--- client/src/context/adventurer/adventurerState.js
+++ client/src/context/adventurer/adventurerState.js
@@ -11,21 +11,20 @@ export default function AdventurerState(props) {
   const [state, dispatch] = useReducer(AdventurerReducer, initialState);
 
   // Retrieve a list of adventurers
-  const retrieveAdventurers = () => {
+  const retrieveAdventurers = () =>
     axios.get("http://localhost:8080/api/adventurers").then((res) => {
-      if (res?.data) dispatch({ appliesTo: "adventurers", data: res?.data });
+      if (res?.data)
+        dispatch({ appliesTo: "adventurers", data: res?.data?.data });
     });
-  };
 
-  // Retrieve a list of adventurers
-  const loginAdventurer = (id) => {
+  // Login as a given adventurer
+  const loginAdventurer = (id) =>
     axios.post(`http://localhost:8080/api/login/${id}`).then(() => {
       dispatch({
         appliesTo: "adventurer",
         data: state.adventurers.find(({ _id }) => id === _id),
       });
     });
-  };
 
   return (
     <AdventurerContext.Provider
--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,5 +1,15 @@
+import { useContext, useEffect } from "react";
 import AdventurerGrid from "./components/adventurerGrid";
+import AdventurerContext from "../../context/adventurer/adventurerContext";
 
 export default function LoginPage() {
-  return <AdventurerGrid adventurers={[]} />;
+  const adventurerContext = useContext(AdventurerContext);
+  const { adventurers, retrieveAdventurers } = adventurerContext;
+
+  useEffect(() => {
+    retrieveAdventurers();
+    // eslint-disable-next-line
+  }, []);
+
+  return <AdventurerGrid adventurers={adventurers} />;
 }
