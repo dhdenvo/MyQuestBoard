2c3ce7454393d745bf16c69e26c41290de8e47c6
dhdenvo <dhd@rogers.com>
2023-08-09 02:10:46 -0400

    Create a generic function for all jobs that allows for logging of the jobs start & end

--- server/jobs/cronHandler.js
+++ server/jobs/cronHandler.js
@@ -1,15 +1,27 @@
 const { CronJob } = require("cron");
 
 const CRONS = [
-  {
-    func: require("./cronFuncs/questFailure"),
-    runtime: "0 50 23 * * *",
-  },
-  { func: require("./cronFuncs/questReminder"), runtime: "0 * * * * *" },
-  { func: require("./cronFuncs/imageCreator"), runtime: "0 0 * * * *" },
+  { path: "./cronFuncs/questFailure", runtime: "0 50 23 * * *" },
+  { path: "./cronFuncs/questReminder", runtime: "0 * * * * *" },
+  { path: "./cronFuncs/imageCreator", runtime: "0 0 * * * *" },
 ];
 
-CRONS.map(({ func, runtime }) => {
+CRONS.forEach(({ path, runtime }) => {
   if (!runtime) return;
+  // Use the cron job's file name as the cron's name
+  const name = path.slice(path.lastIndexOf("/") + 1);
+  // Define a generic function for running all cron jobs
+  const func = async () => {
+    console.log(`Starting cron job ${name}`);
+    // Run the cron job (catching errors)
+    const res = await require(path)().catch((error) => {
+      console.error(`Error in cron job ${name}`);
+      return { error };
+    });
+    if (res?.error) return;
+    console.log(`Completed cron job ${name}`);
+  };
+
+  // Create the cron job with the generic function
   new CronJob(runtime, func, null, true, "America/Toronto");
 });
