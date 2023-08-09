159ec29b938d5293b7ea169364111dc318ecdd20
dhdenvo <dhd@rogers.com>
2023-08-09 01:23:56 -0400

    Add the create ai image to the ai helper

--- server/features/shared/helpers/aiHelper.js
+++ server/features/shared/helpers/aiHelper.js
@@ -1,5 +1,6 @@
 const { openAIConn } = require("../../../global/globalConnections");
 
+// Generate a text response from a single message
 const generateSingleResponse = async (message, systemContext) => {
   const openai = openAIConn();
   const messages = [];
@@ -16,6 +17,18 @@ const generateSingleResponse = async (message, systemContext) => {
   return chatCompletion.data.choices[0].message;
 };
 
+// Generate an ai image url from a link
+const generateAiImage = async (prompt) => {
+  const openai = openAIConn();
+  const response = await openai.createImage({
+    prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+  return response.data.data[0].url;
+};
+
 module.exports = {
   generateSingleResponse,
+  generateAiImage,
 };
