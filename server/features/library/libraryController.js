c6ee7fa7ad72858eae8760b3cbdcc91c3f0ceb9c
dhdenvo <dhd@rogers.com>
2023-08-14 02:13:33 -0400

    Update the get page route to only run path pipeline if a path is given to increase efficiency

--- server/features/library/libraryController.js
+++ server/features/library/libraryController.js
@@ -1,6 +1,7 @@
 const bookModel = require("./bookModel");
 const pageModel = require("./pageModel");
 const { Types } = require("mongoose");
+const { COLLECTION_NAMES } = require("../../global/config.json");
 
 const getBooks = ({ adventurer }) =>
   bookModel.findMany({ adventurer: adventurer._id });
@@ -13,53 +14,57 @@ const savePath = ({ params }) =>
   );
 
 // Get the page along with its next pages with a value if the next pages follow the given path
-const getPage = async ({ params, query }) =>
-  pageModel.aggregate(
+const getPage = async ({ params, query }) => {
+  const nextPagePipeline = [
+    { $match: { prevPage: new Types.ObjectId(params.pageId) } },
+  ];
+  // If the path is passed, append the path searching system
+  if (query.path) {
+    nextPagePipeline.push({
+      $graphLookup: {
+        from: COLLECTION_NAMES.PAGE,
+        startWith: "$nextPages",
+        connectFromField: "nextPages",
+        connectToField: "_id",
+        as: "followsPath",
+      },
+    });
+    nextPagePipeline.push({
+      $addFields: {
+        followsPath: {
+          $or: [
+            {
+              $in: [
+                new Types.ObjectId(query.path),
+                {
+                  $map: { input: "$followsPath", as: "temp", in: "$$temp._id" },
+                },
+              ],
+            },
+            { $eq: [new Types.ObjectId(query.path), "$_id"] },
+          ],
+        },
+      },
+    });
+  }
+  nextPagePipeline.push({
+    $project: { _id: 0, pageTitle: 1, pageNum: 1, followsPath: 1 },
+  });
+
+  return pageModel.aggregate(
     [
       { $match: { _id: new Types.ObjectId(params.pageId) } },
       {
         $lookup: {
           from: COLLECTION_NAMES.PAGE,
-          pipeline: [
-            { $match: { prevPage: new Types.ObjectId(params.pageId) } },
-            {
-              $graphLookup: {
-                from: COLLECTION_NAMES.PAGE,
-                startWith: "$nextPages",
-                connectFromField: "nextPages",
-                connectToField: "_id",
-                as: "followsPath",
-              },
-            },
-            {
-              $addFields: {
-                followsPath: {
-                  $or: [
-                    {
-                      $in: [
-                        new Types.ObjectId(query.path),
-                        {
-                          $map: {
-                            input: "$followsPath",
-                            as: "temp",
-                            in: "$$temp._id",
-                          },
-                        },
-                      ],
-                    },
-                    { $eq: [new Types.ObjectId(query.path), "$_id"] },
-                  ],
-                },
-              },
-            },
-            { $project: { _id: 0, pageTitle: 1, pageNum: 1, followsPath: 1 } },
-          ],
+          pipeline: nextPagePipeline,
           as: "nextPages",
         },
       },
     ],
     false
   );
+};
 
 module.exports = {
   getBooks,
