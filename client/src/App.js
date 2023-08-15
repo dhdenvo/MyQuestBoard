0c18de147c926be20d7a280d29ff964b400a8b21
dhdenvo <dhd@rogers.com>
2023-08-14 20:26:04 -0400

    Add a theme for the entire app Colours in the theme are a WIP

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,37 @@ import QuestState from "./context/quest/questState";
 import AuthHandler from "./context/auth/authHandler";
 import LibraryState from "./context/library/libraryState";
 import AppHeader from "./components/appHeader";
+import { ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    common: {
+      main: "#1976D2",
+      contrastText: "#FFFFFF",
+    },
+    special: {
+      main: "#D1C030",
+      contrastText: "#FFFFFF",
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <AuthHandler>
-        <AdventurerState>
-          <QuestState>
-            <LibraryState>
-              <AppHeader>
-                <PageRouter />
-              </AppHeader>
-            </LibraryState>
-          </QuestState>
-        </AdventurerState>
-      </AuthHandler>
+      <ThemeProvider theme={theme}>
+        <AuthHandler>
+          <AdventurerState>
+            <QuestState>
+              <LibraryState>
+                <AppHeader>
+                  <PageRouter />
+                </AppHeader>
+              </LibraryState>
+            </QuestState>
+          </AdventurerState>
+        </AuthHandler>
+      </ThemeProvider>
     </div>
   );
 }
