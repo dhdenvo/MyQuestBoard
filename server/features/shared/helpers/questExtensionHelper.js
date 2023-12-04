b7ddd25c2090ebf825eda5d97bc2fac5d3337f04
dhdenvo <dhd@rogers.com>
2023-12-03 19:38:29 -0500

    [#11] Create weekday & weekend quests

--- server/features/shared/helpers/questExtensionHelper.js
+++ server/features/shared/helpers/questExtensionHelper.js
@@ -1,27 +1,36 @@
 const { FREQUENCY_TYPES } = require("../configs/questConfig.json");
 const alternateModels = require("./alternateModels");
-const { differenceInDays } = require("date-fns");
+const { differenceInDays, isWeekend } = require("date-fns");
 
 // Auto increase a date until it passes the current date
-const autoIncFunc = (func, date) => {
+const autoIncFunc = (func, date, condFunc = () => true) => {
   const newDate = func(date, 1);
   if (
-    differenceInDays(newDate, new Date(0)) >=
-    differenceInDays(new Date(), new Date(0))
+    differenceInDays(newDate, new Date(0)) >
+      differenceInDays(new Date(), new Date(0)) &&
+    condFunc(newDate)
   )
     return newDate;
-  return autoIncFunc(func, newDate);
+  return autoIncFunc(func, newDate, condFunc);
 };
 // Specific changes to a quest when the quest is completed
-const questCompletionChanges = ({ frequency, dueDate }, isFailure) => {
+const questExtensionChanges = ({ frequency, dueDate }, isFailure) => {
   const changes = {
     [FREQUENCY_TYPES.DAILY]: { dueDate: require("date-fns").addDays },
     [FREQUENCY_TYPES.WEEKLY]: { dueDate: require("date-fns").addWeeks },
     [FREQUENCY_TYPES.MONTHLY]: { dueDate: require("date-fns").addMonths },
     [FREQUENCY_TYPES.YEARLY]: { dueDate: require("date-fns").addYears },
     [FREQUENCY_TYPES.ONCE]: isFailure
-      ? { dueDate: require("date-fns").addWeeks }
+      ? { dueDate: require("date-fns").addDays }
       : { isComplete: true },
+    [FREQUENCY_TYPES.WEEKEND]: {
+      dueDate: require("date-fns").addDays,
+      condFunc: isWeekend,
+    },
+    [FREQUENCY_TYPES.WEEKDAY]: {
+      dueDate: require("date-fns").addDays,
+      condFunc: (date) => !isWeekend(date),
+    },
   };
   let change = changes[frequency];
   // If the frequency is a number, increase by that number of days
@@ -30,12 +39,13 @@ const questCompletionChanges = ({ frequency, dueDate }, isFailure) => {
       dueDate: (date) => require("date-fns").addDays(date, parseInt(frequency)),
     };
   // If increasing the due date, ensure the new due date is after now
-  if (change?.dueDate) change.dueDate = autoIncFunc(change.dueDate, dueDate);
+  if (change?.dueDate)
+    change.dueDate = autoIncFunc(change.dueDate, dueDate, change.condFunc);
   return change;
 };
 
 module.exports = async (quest, isFailure = false) => {
-  const questChanges = questCompletionChanges(quest, isFailure);
+  const questChanges = questExtensionChanges(quest, isFailure);
   // If there is an increase function, update the quest with the new due date
   if (!questChanges) return;
   return await alternateModels.QUEST.updateOne(
