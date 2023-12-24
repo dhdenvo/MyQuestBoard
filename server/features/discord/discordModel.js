ab1e613420dcc4589fad9d056986e1fbe274889d
dhdenvo <dhd@rogers.com>
2023-12-24 17:17:51 -0500

    Add a function to get someone's discord status

--- server/features/discord/discordModel.js
+++ server/features/discord/discordModel.js
@@ -1,5 +1,18 @@
 const { discordConnProm } = require("../../global/globalConnections");
 
+// Get a guild user's presence
+const getPresence = async (adventurer) => {
+  const client = await discordConnProm;
+  const guildMember = await client.guilds.cache
+    .map((guild) => guild.members.fetch(adventurer.discordId))
+    .find((x) => x);
+  return guildMember.presence;
+};
+
+// Get a user's status
+const getStatus = (adventurer) =>
+  getPresence(adventurer).then(({ status }) => status);
+
 // Send a discord direct message to an adventurer
 const sendMessage = async (adventurer, message) => {
   const client = await discordConnProm;
@@ -28,6 +41,7 @@ const handleReactionAdd = async (func) => {
 };
 
 module.exports = {
+  getStatus,
   sendMessage,
   handleDirectMessage,
   handleReactionAdd,
