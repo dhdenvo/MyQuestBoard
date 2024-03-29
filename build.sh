1af0911fd06b1b747fb14bf9617aa35d79329695
dhdenvo <dhd@rogers.com>
2024-03-29 15:34:18 -0400

    Create generic get path function for stored images

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 sudo docker rm $(sudo docker ps -aq) -f
 sudo docker rmi $(sudo docker images -q) -f
 sudo docker build -t quest . --no-cache
-sudo docker run -p 80:8080 quest
+sudo docker run -p 80:8080 quest -v "/home/dhdenvo/Pictures/QuestBoardImages:/savedImages"
